1c59517df1e3071cb81ef654344886ed1356834b
Ahmed <ahmed.fattah98@eng-st.cu.edu.eg>
2020-05-07 22:19:35 +0200

    fetch one operand complete ISA

---
+++ register.vhd
@@ -0,0 +1,23 @@
+Library IEEE;
+USE IEEE.std_logic_1164.all;
+
+entity reg is 
+generic (n:integer := 16);
+port(	clk : in std_logic ; 
+	reset : in std_logic ; 
+	enable : in std_logic ; 
+	d	: in std_logic_vector(n-1 downto 0);
+	q	: out std_logic_vector(n-1 downto 0)
+);
+end entity;
+
+architecture regarc of reg is
+begin
+process (clk,reset,enable)
+begin
+if (reset = '1') then q <= (OTHERS => '0');
+elsif (rising_edge(clk)) and enable = '1' then q <= d;
+end if;
+end process;
+
+end architecture;
